b67129f802757975e1536142afcc728e23263e5d
Sharjeel14 <sharjeelr117@gmail.com>
2023-06-22 15:02:35 +0500

    chnages

--- main.py
+++ main.py
@@ -114,5 +114,5 @@ def get_presentation():
     return jsonify(updatedList)
 
 
-if __name__ == "__main__":
-    application.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    application.run(host="0.0.0.0", port=5000, debug=True)
